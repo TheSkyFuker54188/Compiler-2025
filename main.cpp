4c37c249d9bff49dab5b2f6ccf2ba967c48d8fed
项晓清 <1737175681@qq.com>
2025-08-05 00:46:17 +0000

    fix main.cpp

--- main.cpp
+++ main.cpp
@@ -465,9 +465,9 @@ bool compileFile(const std::string &filename, bool verbose = true,
         asm_file << asm_content;
         // std::cout<< asm_content;
       } else {
-        // asm_file << "reach here ,register_allocation_success="
-        //          << register_allocation_success;
-        asm_file << asm_content;
+        asm_file << "reach here ,register_allocation_success="
+                 << register_allocation_success;
+        //asm_file << asm_content;
       }
       asm_file.close();
       if (verbose) {
